7625a637d7df5de5e10e350e3dd76b5891300ca8
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-11-05 14:49:43 -0300

    Testes e controller de autenticação

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,33 @@
+import { PrismaUsersRepository } from '@/repositores/prisma/prisma-users-repository'
+import { AuthenticateUseCase } from '@/services/authenticate'
+import { InvalidCredentialsError } from '@/services/errors/invalid-credentials-error'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+    const authenticateBodySchema = z.object({
+        email: z.string().email(),
+        password: z.string().min(6),
+    })
+
+    const { email, password } = authenticateBodySchema.parse(request.body)
+
+    try {
+        const usersRepository = new PrismaUsersRepository()
+        const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+
+        await authenticateUseCase.execute({
+            email,
+            password,
+        })
+
+    } catch (err) {
+        if (err instanceof InvalidCredentialsError) {
+            return reply.status(409).send({ message: "Email already exists." })
+        }
+
+        throw err
+    }
+
+    return reply.status(200).send()
+}
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,4 +3,5 @@ import { register } from '@/http/controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', register)
+    app.post('/session', register)
 }
