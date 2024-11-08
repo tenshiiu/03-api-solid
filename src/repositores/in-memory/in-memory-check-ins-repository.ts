7072bf331a67cb743d367f90376c114c61a55c27
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-11-08 11:47:30 -0300

    Caso de uso de histórico

--- src/repositores/in-memory/in-memory-check-ins-repository.ts
+++ src/repositores/in-memory/in-memory-check-ins-repository.ts
@@ -4,7 +4,8 @@ import { randomUUID } from "node:crypto";
 import dayjs from "dayjs";
 
 export class InMemoryCheckInRepository implements CheckInsRepository {
-     public items: CheckIn[] = []
+    
+    public items: CheckIn[] = []
 
     async findByUserIdOnDate(userId: string, date: Date) {
        const startOfTheDay = dayjs(date).startOf('date')
@@ -24,7 +25,13 @@ export class InMemoryCheckInRepository implements CheckInsRepository {
         }
 
        return checkOnSameDate
-    }   
+    }  
+
+    async findManyByUserId(userId: string, page: number) {
+        return this.items
+          .filter((item) => item.user_id == userId)
+          .slice((page - 1) * 20, page * 20)
+    } 
 
     async create(data: Prisma.CheckInUncheckedCreateInput) {
         const checkIn = {
