01b2e32c6f078e717a188375e469346c042bfa47
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-11-13 14:53:06 -0300

    Factories dos casos de uso

---
+++ src/use-cases/factories/make-validate-check-in-use-case.ts
@@ -0,0 +1,9 @@
+import { PrismaCheckInsRepository } from "@/repositores/prisma/prisma-check-in-repository";
+import { ValidateCheckInUseCase } from "../validate-check-in";
+
+export function makeValidateCheckInUseCase() {
+    const checkInsRepository = new PrismaCheckInsRepository()
+    const useCase = new ValidateCheckInUseCase(checkInsRepository)
+
+    return useCase
+}
