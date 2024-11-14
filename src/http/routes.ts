db9f27e7d17494c54d9dc70d73125d535ffb7e9e
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-11-14 15:56:33 -0300

    Controller de perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,10 +3,11 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { profile } from './controllers/profile';
 import { authenticate } from './controllers/authenticate';
+import { verifyJWT } from './middlewares/verify-jwt';
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', register)
     app.post('/session', authenticate)
 
-    app.get('/me', profile)
+    app.get('/me', { onRequest: [verifyJWT]},profile)
 }
