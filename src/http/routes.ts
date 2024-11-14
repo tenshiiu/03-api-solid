7b0b4b4e95c2da02584469b29e94b4720291daa9
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-11-14 14:40:21 -0300

    Princípios JWT no Fastify

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,12 @@
 import { FastifyInstance } from 'fastify'
-import { register } from '@/http/controllers/register'
+
+import { register } from './controllers/register'
+import { profile } from './controllers/profile';
+import { authenticate } from './controllers/authenticate';
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', register)
-    app.post('/session', register)
+    app.post('/session', authenticate)
+
+    app.get('/me', profile)
 }
