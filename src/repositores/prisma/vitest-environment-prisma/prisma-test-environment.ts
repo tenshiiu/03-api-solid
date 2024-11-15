0cb2d29a57dc9225bf727a6c986efaa3c89675f4
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-11-15 08:44:14 -0300

    Teste E2E do registro e Environment

--- src/repositores/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ src/repositores/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,42 @@
-import { Environment } from 'vitest'
+import 'dotenv/config'
 
-export default <Environment> {
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import { Environment } from 'vitest'
+import { disconnect } from 'node:process'
+
+const prisma = new PrismaClient()
+
+function generateDatebaseURL(schema: string) {
+    if (!process.env.DATABASE_URL) {
+        throw new Error('Please provide a DATABASE_URL environment variable.')
+    }
+
+    const url = new URL(process.env.DATABASE_URL)
+
+    url.searchParams.set('schema', schema)
+
+    return url.toString()
+}
+
+export default <Environment>{
     name: 'prisma',
     async setup() {
-        console.log('Executou!'
+        const schema = randomUUID()
+        const databaseURL = generateDatebaseURL(schema)
 
-            return {
-                teardown() {}
+        process.env.DATABASE_URL = databaseURL
+
+        execSync('npx prisma migrate deploy')
+
+        return {
+            async teardown() {
+                await prisma.$executeRawUnsafe(
+                    `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+                )
+                await prisma.$disconnect()
             }
-        )
+        }
     }
-} 
+}
+
